02c2c53ad4c122f4518d738c0f48b5d14176cc70
Leonam Teixeira de Vasconcelos <leonan.aiirrc02t@gmail.com>
2019-10-25 12:11:41 -0300

    Correção nos comentários para melhor visualização

--- acomod2.m
+++ acomod2.m
@@ -1,4 +1,8 @@
 function [ estavel, tempo ]= acomod2 ( t, sys, criterio = 0.02, round_p = 4 )
+  % função acomod2 
+  %
+  % @author: Leonam Teixeira de Vasconcelos
+  %
   % Verifica se o sistema de segunda ordem sys (vetor de pontos) é 
   % estável e se sim, em qual tempo se estabiliza.
   %
